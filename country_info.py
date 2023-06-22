218c6b0827db096e90574ef5a8a0f6d9caf83b3d
Denis2221 <opaopaden@gmail.com>
2023-06-22 14:21:30 +0300

    Initial commit

---
+++ country_info.py
@@ -0,0 +1,25 @@
+import json
+
+# Зчитування даних з JSON-файлу
+with open("countries.json", "r") as file:
+    data = json.load(file)
+
+# Запит назви країни від користувача
+country_name = input("Введіть назву країни: ")
+
+# Пошук країни за назвою у JSON-даних
+for country in data:
+    if country["name"] == country_name:
+        # Отримання необхідної інформації
+        country_name = country["name"]
+        capital_name = country["capital"]
+        region_name = country["region"]
+
+        # Виведення отриманої інформації
+        print("Назва країни:", country_name)
+        print("Назва столиці:", capital_name)
+        print("Назва регіону:", region_name)
+        break
+else:
+    print("Країна не знайдена.")
+
